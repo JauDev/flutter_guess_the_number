ac76513ac205dd8d5ec9336df780db7b7d48c497
JaumeGandara <jaumegandara@gmail.com>
2025-05-04 16:52:52 +0200

    feat: :sparkles: widget number_view.dart

---
+++ lib/widgets/number_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../themes/app_colors.dart';
+
+class NumberView extends StatelessWidget {
+  final String label;
+  final int value;
+  const NumberView({super.key, required this.label, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(label),
+        Container(
+          width: 68,
+          height: 56,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: AppColors.primaryColor.withOpacity(.1),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Text('$value',
+              style: const TextStyle(fontWeight: FontWeight.bold)),
+        ),
+      ],
+    );
+  }
+}
