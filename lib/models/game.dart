022a97d893d0811f1ea87fd4b9a8ef73cc68fa87
JaumeGandara <jaumegandara@gmail.com>
2025-05-04 16:50:25 +0200

    feat: :sparkles: game model

--- lib/models/game.dart
+++ lib/models/game.dart
@@ -1,27 +1,40 @@
 import 'dart:math';
 
 class Game {
-  static final int MIN_VALUE = 0;
-  static final int MAX_VALUE = 100;
+  static const int minValue = 0;
+  static const int maxValue = 100;
 
-  int _targetValue = 0;
-  int _points = 0;
+  final _rng = Random();
+  int _target = 0;
+  int _lastPoints = 0;
+  int _score = 0;
+  int _round = 1;
 
-  int get targetValue => _targetValue;
-  int get points => _points;
+  int get target => _target;
+  int get lastPoints => _lastPoints;
+  int get score => _score;
+  int get round => _round;
 
-  Game() {
-    _targetValue = Random().nextInt(MAX_VALUE + 1 - MIN_VALUE) + MIN_VALUE;
+  Game() { _nextTarget(); }
+
+  void hit(double slider) {
+    final diff = (_target - slider.round()).abs();
+    _lastPoints = maxValue - diff;
+    _score += _lastPoints;
+    _round += 1;
+    _nextTarget();
   }
 
-  void calculatePoints(double sliderValue) {
-    int sliderValueInt = sliderValue.round();
-    int difference = (_targetValue - sliderValueInt).abs();
-    _points = MAX_VALUE - difference;
+  void resetRound() => _nextTarget();
+
+  void restart() {
+    _score = 0;
+    _round = 1;
+    _lastPoints = 0;
+    _nextTarget();
   }
 
-  void reset() {
-    _targetValue = Random().nextInt(MAX_VALUE + 1 - MIN_VALUE) + MIN_VALUE;
-    _points = 0;
+  void _nextTarget() {
+    _target = _rng.nextInt(maxValue + 1 - minValue) + minValue;
   }
 }
